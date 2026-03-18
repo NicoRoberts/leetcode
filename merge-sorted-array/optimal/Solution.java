9412051ccf450e83553a9ad32d1abe90190c5695
Nicolas Roberts <nicolas@dronelink.com>
2026-03-17 19:30:19 -0700

    added optimal java solution

---
+++ merge-sorted-array/optimal/Solution.java
@@ -0,0 +1,25 @@
+// Time Complexity - O(m + n)
+// Space Complexity - O(1)
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        if (n == 0) {
+            return;
+        }
+
+        while (j >= 0) {
+            if (i < 0 || nums2[j] >= nums1[i]) {
+                nums1[k] = nums2[j];
+                j--;
+            } else {
+                nums1[k] = nums1[i];
+                i--;
+            }
+            k--;
+        }
+    }
+}
